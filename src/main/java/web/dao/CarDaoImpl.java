ccd4f8c1533091c751ad911dcd5a610592fad830
Marin039 <115377867+Marin039@users.noreply.github.com>
2023-01-19 20:55:59 +0300

    Add files via upload

---
+++ src/main/java/web/dao/CarDaoImpl.java
@@ -0,0 +1,27 @@
+package web.dao;
+
+import org.springframework.stereotype.Component;
+import web.model.Car;
+
+import java.util.ArrayList;
+import java.util.List;
+@Component
+public class CarDaoImpl implements CarDao{
+    private final List <Car> allCars;
+
+    {
+        allCars = new ArrayList<>();
+        allCars.add(new Car ("Mercedec", 123, "white"));
+        allCars.add(new Car ("BMW", 456, "black"));
+        allCars.add(new Car("Bentley", 789, "red"));
+        allCars.add(new Car("Audi", 321, "grey"));
+        allCars.add(new Car("Lamborghini", 746, "blue"));
+    }
+
+    @Override
+    public List<Car> getAllCars(int count) {
+        return allCars.stream().limit(count).toList();
+    }
+
+
+}
---
+++ src/main/java/web/service/CarServiceImpl.java
@@ -0,0 +1,22 @@
+package web.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import web.dao.CarDao;
+import web.model.Car;
+
+import java.util.List;
+@Component
+public class CarServiceImpl implements CarService{
+    private final CarDao carDao;
+    @Autowired
+    public CarServiceImpl(CarDao carDao) {
+        this.carDao = carDao;
+    }
+    @Override
+    public List<Car> getAllCars(String count1) {
+        int count = Integer.parseInt(count1);
+        return carDao.getAllCars(count);
+    }
+
+}
